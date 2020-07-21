89dec8cf2e3a199511d8028fc1b144b094fcffe1
Walker-TW <thomas.w.walker@live.co.uk>
2020-07-21 23:43:19 +0100

    I created a triangle following a tutorial

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.static("public"));
 
 //listen for it on local port 3200 (I am using 3000 for a react project)
-app.listen(3200);
+app.listen(3000);
 
 //run by runnig node server.js in terminal
