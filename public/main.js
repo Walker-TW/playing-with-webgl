4fa207eeacf61b132b3970d32d984216ce788d07
Walker-TW <thomas.w.walker@live.co.uk>
2020-07-21 22:35:12 +0100

    Web server added using express

--- public/main.js
+++ public/main.js
@@ -1,2 +1,8 @@
 const canvas = document.querySelector("canvas");
 const gl = canvas.getContext("webgl");
+
+// makes sure that if webgl is not supported via browser then it will throw an error
+
+if (!gl) {
+  throw new Error("WebGL not supported on your browser");
+}
